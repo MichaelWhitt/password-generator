08f2b6d5cfd58b4d6702871a24f5414b24f6a7d0
MichaelWhitt <michaelwhitt90@gmail.com>
2022-03-05 23:17:54 -0600

    fix(pw): add switch logic

--- src/Password/PasswordForm.js
+++ src/Password/PasswordForm.js
@@ -5,35 +5,50 @@ import styles from './password.module.css'
 export default class PasswordForm extends React.Component {
 state = {
     password: '',
-    passwordOptions : [1,2,3,4,5,6,7,8,9,0, 'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J',
-'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S',
-'t', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z', '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=']
+    lower: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r','s',' t', 'u', 'v', 'w', 'x', 'y', 'z'],
+    upper: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z'],
+    symbols: ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '='],
+    numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 }
 
-
-
 choosePW = (max) => {
-    const injectSym = () => {
-        // include logic to inject symbol
+    const {lower, upper, symbols, numbers, password} = this.state
+    let pwCode = ''
+
+    
+    // if pw === 8 lower, upper, lower, sym, num, sym, upper
+    // if pw === 10 lower, upper, lower, sym, num, sym, upper, num, lower, upper
+    // if pw === 12 lower, upper, lower, sym, num, sym, upper, lower, num, upper, sym, lower
+
+    for (let i = 0; i < max; i++){
+        let choice = Math.floor(Math.random() * 4) +1
+        let randomLower = Math.floor(Math.random() * 26)
+        let randomUpper = Math.floor(Math.random() * 26)
+        let randomSymbol = Math.floor(Math.random() * 14)
+        let randomNum = Math.floor(Math.random() * 10)
+        switch (choice) {
+            case 1:pwCode += lower[randomLower]
+                break;
+            case 2: pwCode += upper[randomUpper]
+                break;
+            case 3: pwCode += symbols[randomSymbol]
+                break;
+            case 4: pwCode += numbers[randomNum]
+                break;
+            default:
+                break;
+        }
     }
 
-    let pwCode = ''
-    for (let i = 0; i < max; i++){
-        let randomNum = Math.floor(Math.random() * 76)
-        pwCode += this.state.passwordOptions[randomNum]
-        injectSym()
-        this.setState({password: pwCode})
-    }
+    this.setState({password: pwCode})
 }
 
     render(){
-
         return(
             <div className={`${styles.container}`} style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                 <div className={`${styles.pwBox}`}>
                     <div className={`${styles.pwDisplay}`}>{this.state.password}</div>
                     <button style={{width: '20%'}}>Copy</button>
-                    <button onClick={() => this.choosePW(6)}>Generate 6 Digit Password</button>
                     <button onClick={() => this.choosePW(8)}>Generate 8 Digit Password</button>
                     <button onClick={() => this.choosePW(10)}>Generate 10 Digit Password</button>
                     <button onClick={() => this.choosePW(12)}>Generate 12 Digit Password</button>
