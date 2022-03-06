3e6a24fec5d9ac6a55a35dcd413d03cd88fe464d
MichaelWhitt <michaelwhitt90@gmail.com>
2022-03-06 10:52:41 -0600

    feat(copy): add copy button functionality

--- src/Password/PasswordForm.js
+++ src/Password/PasswordForm.js
@@ -5,10 +5,18 @@ import styles from './password.module.css'
 export default class PasswordForm extends React.Component {
 state = {
     password: '',
-    lower: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r','s',' t', 'u', 'v', 'w', 'x', 'y', 'z'],
+    lower: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
     upper: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z'],
     symbols: ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '='],
     numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+}
+
+handleChange = (e) => {
+    this.setState({password: e.target.value})
+}
+
+onCopy = () => {
+    navigator.clipboard.writeText(this.state.password)
 }
 
 choosePW = (max) => {
@@ -47,8 +55,8 @@ choosePW = (max) => {
         return(
             <div className={`${styles.container}`} style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                 <div className={`${styles.pwBox}`}>
-                    <div className={`${styles.pwDisplay}`}>{this.state.password}</div>
-                    <button style={{width: '20%'}}>Copy</button>
+                    <input type='text' value={this.state.password} onChange={this.handleChange} className={`${styles.pwDisplay} password-display`}/>
+                    <button style={{width: '20%'}} onClick={this.onCopy}>Copy</button>
                     <button onClick={() => this.choosePW(8)}>Generate 8 Digit Password</button>
                     <button onClick={() => this.choosePW(10)}>Generate 10 Digit Password</button>
                     <button onClick={() => this.choosePW(12)}>Generate 12 Digit Password</button>
@@ -56,5 +64,4 @@ choosePW = (max) => {
             </div>
         )
     }
-
 }
